5a34b82f5d13c79926abaa918f6a2d9c4550ea93
hussein asadi <artistpenman1515@gmail.com>
2025-06-28 10:21:49 -0700

    more cpp main added

--- day4/main.cpp
+++ day4/main.cpp
@@ -14,9 +14,14 @@ int main()
     // auto sizeType = sizeof(double);
     // auto sizeType = sizeof(long);
     // auto sizeType = sizeof(bool);
-    auto sizeType = sizeof(long double); 
+    auto sizeType = sizeof(long double);
     cout << "size of types: " << sizeType << endl;
     cout << "minimum number: " << numeric_limits<int>::min() << endl;
     cout << "maximum number: " << numeric_limits<int>::max() << endl;
-    return 0;
+    int myArray[] = {10, 20, 30, 40, 50};
+
+    for (int element : myArray)
+    {
+        cout << element << " ";
+    }
 }
